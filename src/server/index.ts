2dd0a100c45f62b2e6fb6785b918b957d828b73a
M. Nindra Zaka <mnindrazaka@gmail.com>
2022-12-30 09:50:05 +0700

    feat: setup webpack config for server

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,10 +3,10 @@ import path from "path";
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "..", "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(port, () => {
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  entry: "./dist/client/index.js",
-  output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "public"),
-  },
-};
